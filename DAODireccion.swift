761f2a19d70a2eab380422e0627b9a92a226bfb7
Isabel Dunin Borkowski <IsabelDuninBorkowski@Mac-mini-de-Isabel.local>
2015-08-14 12:40:18 -0500

    Archivos de DAO creados

---
+++ DAODireccion.swift
@@ -0,0 +1,14 @@
+//
+//  DAODireccion.swift
+//  Tiempos
+//
+//  Created by Isabel Dunin Borkowski on 14/08/15.
+//  Copyright (c) 2015 Isabel Dunin-Borkowski. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class daoDireccion{
+    
+}
