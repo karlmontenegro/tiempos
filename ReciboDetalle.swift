d541b7efa8751d944d8ee2c644841673b977237b
Isabel Dunin Borkowski <IsabelDuninBorkowski@mac-mini-de-isabel.local>
2016-02-24 11:21:36 -0500

    recibo detalle added

---
+++ ReciboDetalle.swift
@@ -0,0 +1,17 @@
+//
+//  ReciboDetalle.swift
+//  
+//
+//  Created by Isabel Dunin Borkowski on 24/02/16.
+//
+//
+
+import Foundation
+import CoreData
+
+
+class ReciboDetalle: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
